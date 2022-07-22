62310c1d47c57ab4a6c81cb696f627328928c301
ivanazf <ivana.zf93@gmail.com>
2022-07-22 20:16:17 -0300

    modificaciones Desafío menu ecommerce

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <div>
+                <h1>MIBAF para todos</h1>
+            </div>
+            <div>
+                <button>Sweaters</button>
+                <button>Ropa Interior Mujeres</button>
+                <button>Ropa Interior Hombres</button>
+            </div>
+        </nav>
+    )
+}
+
+export default  Navbar
